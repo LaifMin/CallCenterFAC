9bb6a329ecc18f5ab170d9d6640de57ffe8e5ba7
Your Name <you@example.com>
2025-02-28 12:47:03 +0100

    primo commit

---
+++ src/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+
+    private String nome;
+    private String cognome;
+    private String codice;
+    private String indirizzo;
+    private String citta;
+
+
+
+    public Cliente(String nome, String cognome, String codice, String indirizzo, String citta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codice = codice;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+    }
+
+
+    
+
+
+    
+    
+}
